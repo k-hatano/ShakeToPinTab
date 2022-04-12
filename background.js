458fc0a4e7ca5eb7d8f7ebda3092c8afe4f128fb
k-hatano <kent.ruffle.mgj626@gmail.com>
2022-04-13 00:39:56 +0900

    処理見直し、権限見直し、バージョンv1.1に

--- background.js
+++ background.js
@@ -16,7 +16,19 @@ chrome.tabs.onMoved.addListener(function(id, info) {
 			movedTimes = 0;
 			chrome.tabs.get(tmpMovedTabs, function(tab){
 				var pinned = tab.pinned;
-				chrome.tabs.update(tab.id, { 'pinned':!pinned });
+				function togglePinTab(n) {
+					if (n == 0) {
+						return;
+					}
+					chrome.tabs.update(tab.id, { 'pinned':!pinned }, function(newTab){
+						if (newTab == undefined || newTab.pinned == pinned) {
+							setTimeout(function(){
+								togglePinTab(n - 1);
+							}, 100);
+						}
+					});
+				}
+				togglePinTab(100);
 			});
 		}
 	} else {
